8cc73f31759ecdab81385be2a35414b420a82a24
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-06-15 09:13:15 -0700

    Filter the pokeArray, add local state

--- src/App.js
+++ src/App.js
@@ -4,23 +4,35 @@ import Cell from './Cell';
 import pokemonMetadata from 'pokemon-metadata';
 
 const pokeArray = Object.keys(pokemonMetadata);
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchTerm: "" }
+  }
   render() {
     return (
-      <div className="App">
-        {
-          pokeArray.map((poke)=>{
-            return(
-              <Cell
-                key={pokemonMetadata[poke].id}
-                name={pokemonMetadata[poke].name}
-                id={pokemonMetadata[poke].id}
-                sprite={pokemonMetadata[poke].sprites.front_default}
-              />
-            )
-          })
-        }
+      <div>
+        <input name="searchBar" onChange={(e) => {
+            this.setState({searchTerm: e.target.value})
+          }}/>
+        <div className="App">
+          {
+            pokeArray
+              .filter((poke)=> {
+                return pokemonMetadata[poke].name.includes(this.state.searchTerm)
+              })
+            .map((poke)=>{
+              return(
+                <Cell
+                  key={pokemonMetadata[poke].id}
+                  name={pokemonMetadata[poke].name}
+                  id={pokemonMetadata[poke].id}
+                  sprite={pokemonMetadata[poke].sprites.front_default}
+                  />
+              )
+            })
+          }
+        </div>
       </div>
     );
   }
